bf64cb7cb2d58e9d1884cab7749b61a8bd426448
uenoeki54 <uenoeki54ban@gmail.com>
2023-07-08 09:41:56 +0200

    On arrive a uploader une image

--- FrontEnd/assets/addpicture.js
+++ FrontEnd/assets/addpicture.js
@@ -17,6 +17,7 @@ document.getElementById("new-project-form").appendChild(
         console.log("Done");
     })
 );*/
+
 const imageInput = document.getElementById('image-input');
 //variable globale
 var uploadedImage = "";
@@ -39,4 +40,50 @@ imageInput.addEventListener('change', function(){
     reader.readAsDataURL(this.files[0]);
 }
 )
+const uploadForm = document.getElementById("new-project-form");
+const picture = document.getElementById("image-input");
+const title = document.getElementById("title");
+const category = document.getElementById("category");
+const token = localStorage.getItem('Token');
+var formData = new FormData();
+//DEFINITION DE LA FONCTION D'UPLOAD APRES ON LA DEPLACERA PEUT ETRE 
+async function uploadProject(formData) {
+    return fetch('http://localhost:5678/api/works', {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`
+        },
+        body: formData,
+    })
+}
+
+
+//ON ESSAYE D U PLOADER UN NOIVEAU PROJET AU CLICK SUR LE BOUTON
+uploadForm.addEventListener('submit', (e) =>{
+e.preventDefault();
+console.log('vous avez cliqued pour soumettre le formulaire');
+let uploadPicture = picture.files[0];
+let uploadTitle = title.value;
+let uploadCategory = category.value;
+const formData = new FormData();    
+formData.append('image', uploadPicture)
+formData.append('title', uploadTitle)
+formData.append('category', uploadCategory)
+//acces au token stocké dans localstorage lors du login 
+
+uploadProject(formData)
+.then ((response) => response.json())
+.then(valider => {
+    if (valider) {
+        console.log("ok")
+    } else {
+        console.log("error")
+    }
+    }
+)
+
+
+});
+
+
 
